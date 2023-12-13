7a76fa776affb2361d780645e7149751d65c5317
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-12-13 18:06:01 +0530

    Get request done

--- webapp/Frontend-react/server/index.js
+++ webapp/Frontend-react/server/index.js
@@ -3,12 +3,18 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
 const app = express();
+const { aiData } = require('./controllers/aiData')
 
 app.get('/', (req, res) => {
     res.send("<h1> FL-BLOCKCHAIN </h1>")
 })
 
+
+app.get('/aiData', aiData);
+
+
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
     console.log(`App is running on ${port}`)
 })
+
