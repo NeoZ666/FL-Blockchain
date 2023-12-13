23925db399418cab66305afc712a4a12801a1c79
Nilanchal <nilanchalpanda2003@gmail.com>
2023-12-13 23:25:26 +0530

    POST REQUEST done

--- webapp/Frontend-react/server/util/admin.js
+++ webapp/Frontend-react/server/util/admin.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("C:/Users/shweta/Desktop/Pyara sa project/FL-Blockchain/webapp/Frontend-react/server/PrivateKey.json");
+var serviceAccount = require("C:/Users/Nilanchala Panda/Desktop/NILANCHALA_CODING/PROJECTS/FL-Blockchain-LDI/webapp/Frontend-react/server/PrivateKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
