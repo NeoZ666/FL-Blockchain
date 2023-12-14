804e1d4080ba57fc05101ae9798c08f58f903571
neoz666 <neoz.blockchain@gmail.com>
2023-12-14 13:46:47 +0530

    Solidity2 part for sending links to wallet

---
+++ solidity2/index.js
@@ -0,0 +1,28 @@
+import contractABI from "./abi.json";
+// const { default: Web3 } = require("web3");
+
+const contractAddress = '0x68a14B3a0Ec10D56bc8040B379373f810C476871'; 
+
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+const ethereum = new MetaMaskSDK({})
+
+async function sendLinkToWallet(walletAddress) {
+    try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        const txn = await contract.methods.sendToUser(walletAddress).send({ from: account });
+        contract.events.LinkSent({ filter: { to: walletAddress } })
+        .on('data', (event) => {
+            console.log("Custom link sent: ", event.returnValues._text);
+        })
+        .on('error', (error) => {
+            console.error("Event error:", error);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const recipientAddress = "0x68a14B3a0Ec10D56bc8040B379373f810C476871"; 
+sendLinkToWallet(recipientAddress);
