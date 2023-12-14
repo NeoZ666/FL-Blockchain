e81ddc4aafad6d902168af8d2212045725c511f7
neoz666 <neoz.blockchain@gmail.com>
2023-12-14 14:38:13 +0530

    updates

--- solidity2/index.js
+++ solidity2/index.js
@@ -1,6 +1,7 @@
-// import contractABI from "./abi.json";
-const contractABI = require("./abi.json");
-const Web3 = require('web3');
+import contractABI from "./abi.json";
+import Web3 from 'web3';
+// const contractABI = require("./abi.json");
+// const Web3 = require('web3');
 
 const contractAddress = '0x68a14B3a0Ec10D56bc8040B379373f810C476871'; 
 
@@ -9,18 +10,24 @@ const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 async function sendLinkToWallet(walletAddress) {
     try {
+        console.log('Attempting to send link to wallet:', walletAddress);
+
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         const account = accounts[0];
+        console.log('Account selected:', account);
+
         const txn = await contract.methods.sendToUser(walletAddress).send({ from: account });
+        console.log('Transaction sent:', txn);
+
         contract.events.LinkSent({ filter: { to: walletAddress } })
-        .on('data', (event) => {
-            console.log("Custom link sent: ", event.returnValues._text);
-        })
-        .on('error', (error) => {
-            console.error("Event error:", error);
-        });
+            .on('data', (event) => {
+                console.log("Custom link sent: ", event.returnValues._text);
+            })
+            .on('error', (error) => {
+                console.error("Event error:", error);
+            });
     } catch (error) {
-        console.log(error);
+        console.error('Error:', error);
     }
 }
 
